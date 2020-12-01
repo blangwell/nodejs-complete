2c5630cd2053480ffaccae43880bad779ee0f503
blangwell <b.langwell@outlook.com>
2020-12-01 11:04:05 -0800

    [js] Add primitiveVsReference.js, jsRefresher.js, and .gitignore

--- firstApp.js
+++ firstApp.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
 fs.writeFileSync('hello.txt', 'Hello from node (and fs)');
+console.log('why hello!');
---
+++ primitiveVsReference.js
@@ -0,0 +1,49 @@
+let name = 'Barent';
+console.log(name);
+
+let secondName = name;
+console.log(secondName);
+
+// secondName will log 'Barent'
+// primitive values are copied by value
+// and 'pasted' into the other variable
+// when you reassign name, secondName has a different value
+// it references the initial, 'pasted' value
+name = 'Not Barent';
+console.log(secondName);
+
+let person = {
+  age: 31,
+  name: 'Barent',
+  hobbies: ['Coding', 'Video Games', 'Harassing the cat']
+};
+let thirdPerson = {
+  age: 31,
+  name: 'Barent',
+  hobbies: ['Coding', 'Video Games', 'Harassing the cat']
+};
+
+console.log(person);
+
+// var secondPerson = person;
+// Object.assign allows you to merge two objects
+// gathers the properties of the person object and merges into secondPerson
+// create new object copying properties of the old one, not deep clone
+// Object.assign({newObject}, oldObject)
+let secondPerson = Object.assign({}, person);
+console.log(secondPerson);
+
+// secondPerson.name will log 'Not Barent'
+
+// with slice() we can create a real copy, not just the pointer
+let myHobbies = person.hobbies.slice();
+
+person.name = 'Not Barent'; 
+person.hobbies.push('Bein cool');
+console.log(secondPerson);
+
+console.log(thirdPerson);
+
+// let myHobbies = person.hobbies;
+console.log(myHobbies);
+
