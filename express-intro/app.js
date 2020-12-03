4cb5d1a543c636508a5370d47cc7b54b6cfd778b
blangwell <b.langwell@outlook.com>
2020-12-03 15:01:36 -0800

    [express] Add public/css/ and /util

--- express-intro/app.js
+++ express-intro/app.js
@@ -10,13 +10,15 @@ const app = express();
 
 // urlencoded() function calls next() automatically
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // router object is a valid callback
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// use so it will work on all types of requests
+// app.use so it will work on all types of requests
 app.use((req, res, next) => {
+  // we use path.join() so our code will work on different OS
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
--- express-intro/routes/shop.js
+++ express-intro/routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   // using path.join works on linux, mac, and windows
   // build a path that goes routes folder => back one => views => shop
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
