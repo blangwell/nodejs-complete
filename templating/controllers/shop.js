6e26749bc005767ddb35c41b6cc89a48d8f3ba86
blangwell <b.langwell@outlook.com>
2020-12-06 15:25:59 -0800

    [cart/details] Add cart partial and refactor views, add details page

--- templating/controllers/shop.js
+++ templating/controllers/shop.js
@@ -39,6 +39,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  // productId defined in hidden input name= 
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     pageTitle: 'Checkout',
