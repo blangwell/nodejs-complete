1683215c4c50977aeebfc30b235d5c132457b4f7
blangwell <b.langwell@outlook.com>
2020-12-10 14:16:15 -0800

    [section9] Add update and delete product capabilities

--- templating/models/cart.js
+++ templating/models/cart.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -35,6 +36,35 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
+    });
+  }
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = {...JSON.parse(fileContent) };
+      const product = updatedCart.products.findIndex(prod => prod.id === id);
+      if (!product) {
+        return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      })
+    });
+  }
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
